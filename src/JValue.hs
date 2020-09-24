6fca3d80faf37acf902cd98f0939257b3c726256
Julian Jelfs <julian.jelfs@ef.com>
2020-09-24 16:12:42 +0100

    starting point

--- src/JValue.hs
+++ src/JValue.hs
@@ -1,24 +1,5 @@
 module JValue (JValue(..)) where
 
-import           Data.List (intercalate)
-import           Data.Map  as M
-
 data JValue
-    = JString String
-    | JBool Bool
-    | JNum Double
-    | JNull
-    | JArray [JValue]
-    | JObject (M.Map String JValue)
+    = undefined
 
-instance Show JValue where
-    show = showJValue
-
-showJValue (JString s)   = show s
-showJValue (JBool True)  = show "true"
-showJValue (JBool False) = show "false"
-showJValue (JNum n)      = show n
-showJValue JNull         = show "null"
-showJValue (JArray a)    = "[" <> intercalate ", " (show <$> a) <> "]"
-showJValue (JObject o)   = "{" <> intercalate ", " (showKeyVal <$> M.toList o) <> "}"
-    where showKeyVal (k, v) = k <> " : " <> show v
